89b39e6be8488d5fdaadca7d6c6cc380fe7e05f9
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-16 06:54:27 +0600

    localstorage cart check

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const addItem = () => {
     const input = document.getElementById('input');
     const name = input.value;
-    const products = document.getElementById('products');
-    const product = document.createElement('li');
-    products.appendChild(product);
+    displayProduct(name);
+
     // add cart 
 
     // add storage 
 
     input.value = '';
 }
+
+const displayProduct = (name) => {
+    const products = document.getElementById('products');
+    const product = document.createElement('li');
+    product.innerText = name;
+    products.appendChild(product);
+};
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObject;
+    if (cart) {
+        cartObject = JSON.parse(cart);
+    }
+    else {
+        cartObject = {};
+    }
+    return cartObject;
+};
+
