0055f33d4b620c42ea77ea5b05ab02e90d80513c
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-16 07:40:57 +0600

    remove data storage and ui by place order

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+// get item form client 
 const addItem = () => {
     const input = document.getElementById('input');
     const name = input.value;
-
+    if (name === '') {
+        return;
+    };
     // add to ui 
     displayProduct(name);
-
     // add to local storage
     addToCart(name);
-
+    // clear input value 
     input.value = '';
-}
-
+};
+// display product on ui 
 const displayProduct = (name) => {
     const products = document.getElementById('products');
     const product = document.createElement('li');
     product.innerText = name;
     products.appendChild(product);
 };
-
+// get element from storage 
 const getCart = () => {
     const cart = localStorage.getItem('cart');
     let cartObject;
@@ -26,21 +28,31 @@ const getCart = () => {
     }
     else {
         cartObject = {};
-    }
+    };
     return cartObject;
 };
-
+// set to storage data 
 const addToCart = (name) => {
     const cart = getCart();
-    cart[name] = 1;
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    };
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 };
-
+// load previous data from storage 
 const loadFromStorage = () => {
     const cart = getCart();
     for (const item in cart) {
         displayProduct(item);
-    }
-}
+    };
+};
 loadFromStorage();
+// place order and remove data from ui and storage 
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+};
